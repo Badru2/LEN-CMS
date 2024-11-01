654c8a71aa4c5b0d37d6669cd9b93aa7240b1747
Badru2 <hilalbadru4@gmail.com>
2024-11-01 21:08:16 +0700

    feat: add textarea props (atoms)

--- src/components/atoms/input/input.jsx
+++ src/components/atoms/input/input.jsx
@@ -1,14 +1,15 @@
 const StyledInput = ({ type, name, placeholder, value, onChange, className }) => {
-  return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className={`w-full py-3 px-2 rounded shadow-lg focus:outline-none focus:shadow-outline border border-gray-300 ${className}`}
-    />
-  );
+  const props = {
+    type,
+    name,
+    placeholder,
+    value,
+    onChange,
+    className: `w-full py-3 px-2 rounded shadow-lg focus:outline-none focus:shadow-outline 
+                border border-gray-300 ${className} ${type === "textarea" ? "h-full" : ""}`,
+  };
+
+  return <div>{type === "textarea" ? <textarea {...props} /> : <input {...props} />}</div>;
 };
 
 const Input = ({ type, name, placeholder, value, onChange, className }) => {
